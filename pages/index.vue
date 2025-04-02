34b55a038c619798a253f1d86c114e6d8ada2659
HyfImposter <hyfimposter@cau.edu.cn>
2025-04-02 19:47:06 +0800

    updated css style for index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="flex min-h-screen items-center justify-center">
     <div class="container mx-auto text-center">
-      <h1 class="text-4xl font-bold">Hi, I am Jxson, the future fullstack developer</h1>
-      <p class="mt-4 mb-6">I am developing a series of fun tools, stay tuned~</p>
+      <h1 class="text-6xl font-bold">
+        <span class="group hover:animate-bounce inline-block">Hi</span>, I am Jxson, the 
+        <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 animate-gradient-x font-extrabold hover:animate-pulse">
+          fullstack
+        </span> 
+        developer
+      </h1>
+      <p class="text-xl mt-4 mb-6 italic font-serif">I am developing a series of fun tools, stay tuned~</p>
       <NuxtLink to="/waitlist">
-        <UButton class="bg-opacity-80 hover:bg-opacity-100 transition-all">Subscribe Now!</UButton>
+        <UButton class="px-6 py-3 text-lg rounded-full bg-opacity-90 hover:bg-opacity-80 transition-all mt-12", variant="outline">Subscribe Now!</UButton>
       </NuxtLink>
     </div>
   </div>
