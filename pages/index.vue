3099deab2710ab7d4c20b5dabd37fc777e521b20
HyfImposter <hyfimposter@cau.edu.cn>
2025-04-02 19:30:48 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center">
+    <div class="container mx-auto text-center">
+      <h1 class="text-4xl font-bold">Hi, I am Jxson, the future fullstack developer</h1>
+      <p class="mt-4 mb-6">I am developing a series of fun tools, stay tuned~</p>
+      <NuxtLink to="/waitlist">
+        <UButton class="bg-opacity-80 hover:bg-opacity-100 transition-all">Subscribe Now!</UButton>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ pages/waitlist.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="mx-auto text-center font-bold text-5xl flex min-h-screen items-center justify-center">
+    <h1>HI, welcome here, I am here to notice you, you have subscribed successfully!</h1>
+  </div>
+</template>
